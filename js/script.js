9ea81f0b4b14c906c311f843e48ced64f908076b
Eugene Kornilov <eugene.k@stock-pro.net>
2024-12-29 23:45:36 +0300

    Добавление формы в модалку

--- js/script.js
+++ js/script.js
@@ -39,5 +39,51 @@ $(document).ready(function(){
     $('.nav__burger').click(function() {
       $('.burger').slideToggle(300, "swing");
       $("body").toggleClass("active");
-    });  
+    });
+
+    // Обработчик отправки формы
+    $("#requestForm").on("submit", function (e) {
+        e.preventDefault(); // Предотвращаем стандартное поведение (закрытие формы)
+
+        // Блокируем кнопку submit, чтобы предотвратить повторные клики
+        const submitButton = $(this).find('button[type="submit"]');
+        submitButton.prop("disabled", true); // Блокируем кнопку
+
+        // Сбор данных из формы
+        const formData = {
+            surname: $("#requestForm input[name='surname']").val(),
+            name: $("#requestForm input[name='name']").val(),
+            // Такого в форме нет
+            // middleName: $("#requestForm input[name='middleName']").val(),
+            directionType: $("#requestForm input[name='direction']:checked").map(function () {
+                return $(this).val();
+            }).get(),
+            email: $("#requestForm input[name='email']").val(),
+            loginTg: $("#requestForm input[name='telegram']").val(),
+            phoneNumber: $("#requestForm input[name='phone']").val()
+        };
+
+        // Логирование данных для проверки
+        console.log(formData);
+
+        // Отправка данных на сервер
+        $.ajax({
+            // TODO: Вставить свой url
+            url: "https://localhost:8080",
+            method: "POST",
+            data: formData,
+            success: function (response) {
+                console.log("Данные отправлены успешно", response);
+                // TODO: Ниже код просто закроет модалку, если запрос успешно отправлен, а так сделаете уведомление или что-то подобное
+                // $(".popup").removeClass("active");
+            },
+            error: function (error) {
+                console.error("Ошибка при отправке данных", error);
+            },
+            complete: function () {
+                // Включаем кнопку обратно после завершения отправки (успех или ошибка)
+                submitButton.prop("disabled", false);
+            }
+        });
+    });
 });
